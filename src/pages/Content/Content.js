67141e72f50e3efc59f6c242fe3e867af7b06150
phucnguyen <phucnguyen5102003@gmail.com>
2022-07-20 15:47:04 +0700

    test git

--- src/pages/Content/Content.js
+++ src/pages/Content/Content.js
@@ -1,11 +1,19 @@
 import styles from './Content.module.scss'
 import classNames from 'classnames/bind';
+import { useSelector } from 'react-redux';
 
 const cx = classNames.bind(styles)
 
 function Content() {
+    const users = useSelector(state => state.user.users)
+    console.log(users)
     return ( 
-        <h1 className={cx('title')}>Content page</h1>
+        <div>
+            <h1 className={cx('title')}>Content page</h1>
+            <ul>
+
+            </ul>
+        </div>
      );
 }
 
